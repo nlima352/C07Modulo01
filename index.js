a30288d56a30b0dc5404afb64e1048ef461e55da
Natalia Lima <81334149+nlima352@users.noreply.github.com>
2021-08-18 20:07:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//let aluno = prompt('Informe o nome do Aluno');
+//let serie = prompt('Informe a serie do Aluno');
+
+//console.log('O '+aluno+' está na turma '+serie);
+
+let comparaNumero = 6 === 3;
+let comparaTexto = "Texto" === "Outro";
+console.log(comparaNumero);
+console.log(comparaTexto);
